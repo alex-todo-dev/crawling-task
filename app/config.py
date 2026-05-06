e432c8f787b9a7c5c6bded2c283ab2086865640d
codeinsky <todorov@bezeqint.net>
2026-05-07 01:02:15 +0300

    first commit

---
+++ app/config.py
@@ -0,0 +1,15 @@
+CONFIG = {
+"scan_id": "scan_123",
+"login_url": "http://localhost:8080/login.php",
+"start_url_after_login": "http://localhost:8080/index.php",
+"allowed_domains": ["localhost:8080"],
+"max_depth": 3,
+"max_pages": 100,
+"concurrency": 3,
+"login_steps": [
+    {"selector": "[name='username']", "value": "admin"},
+    {"selector": "[name='password']", "value": "password"}
+],
+"submit_selector": "[name='Login'][type='submit']",
+"exclude_patterns": ["logout", "delete", "remove"]
+}
